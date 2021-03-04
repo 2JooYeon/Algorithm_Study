565143022cefa059ed9b93575a002d07e36c2adb
InKyeong <hwang0809@cau.ac.kr>
2021-03-05 00:53:10 +0900

    Programmers_위장_retry

--- InKyeong/5.py
+++ InKyeong/5.py
@@ -15,3 +15,13 @@ def solution(clothes):
         answer *= (i+1)
         
     return answer-1
+
+
+###################################################
+# import collections
+# from functools import reduce
+
+# def solution(clothes):
+#     return reduce(lambda x, y : x*y,a+1 for a in collections.Counter([x[1] for x in clothes] )] )
+# reduce 로 [] 안에 값 다 반복
+# collections로 같은 종류의 옷 숫자 세주기
