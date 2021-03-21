af15dac1e41e43fb43d1a095f5639d2ea6644a04
2JooYeon <lewndus7749@naver.com>
2021-03-21 19:13:26 +0900

    이것이 취업을 위한 코딩테스트다_큰 수의 법칙_success_upgrade version

--- JooYeon/이것이 취업을 위한 코딩테스트다 with 파이썬_Q. 큰 수의 법칙.py
+++ JooYeon/이것이 취업을 위한 코딩테스트다 with 파이썬_Q. 큰 수의 법칙.py
@@ -18,3 +18,21 @@ for i in range(0, M):
     answer += first
 
 print(answer)
+
+N, M, K = map(int, input().split())
+num = list(map(int, input().split()))
+
+num.sort(reverse=True)
+
+first = num[0]
+second = num[1]
+
+# 가장 큰 수가 더해지는 횟수 구하기
+count = int(M/(K+1)) * K
+count += M % (K+1)
+
+answer = 0
+answer += count * first # 가장 큰 수 더하기
+answer += (M - count) * second # 두 번째로 큰 수 더하기
+
+print(answer)
