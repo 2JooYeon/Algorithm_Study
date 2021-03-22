7f69842452573f5cdad199850a2de2eb8b247f73
2JooYeon <lewndus7749@naver.com>
2021-03-22 22:18:53 +0900

    Programmers_큰 수 만들기_success

--- JooYeon/programmers_Q42883.py
+++ JooYeon/programmers_Q42883.py
@@ -1,15 +1,28 @@
+# def solution(number, k):
+#     answer = ''
+#     select = len(number) - k
+#     index = 0
+#
+#     while 1:
+#         target = max(number[index:len(number) - select + 1])
+#         answer += target
+#         index = number[index:len(number) - select + 1].find(target) + 1 + index
+#         select -= 1
+#         if index == len(number) - select:
+#             answer += number[index:]
+#             return answer
+#         elif select == 0:
+#             return answer
+
 def solution(number, k):
     answer = ''
-    select = len(number) - k
-    index = 0
-
-    while 1:
-        target = max(number[index:len(number) - select + 1])
-        answer += target
-        index = number[index:len(number) - select + 1].find(target) + 1 + index
-        select -= 1
-        if index == len(number) - select:
-            answer += number[index:]
-            return answer
-        elif select == 0:
-            return answer
+    data = [number[0]]
+    for num in number[1:]:
+        while len(data) > 0 and data[-1] < num and k > 0:
+            k -= 1
+            data.pop()
+        data.append(num)
+    if k > 0:
+        data = data[:len(number)-k]
+    answer = ''.join(data)
+    return answer
