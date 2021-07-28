364aa0274a0e907e7266ba32ca8c027d5649b115
InKyeongBae <hwang0809@cau.ac.kr>
2021-07-28 22:48:40 +0900

    백준_Q2805_나무자르기_success

--- InKyeong/2021하반기/binarysearch/210728_백준_Q2805_나무자르기.py
+++ InKyeong/2021하반기/binarysearch/210728_백준_Q2805_나무자르기.py
@@ -1,26 +1,44 @@
-
 n, m = map(int, input().split())
 
 tree = list(map(int, input().split()))
+#
+# def branch(target) :
+#     branch = 0
+#     for i in range(0, n):
+#         if tree[i] <= target :
+#             continue
+#         elif tree[i] > target :
+#             branch += (tree[i] - target)
+#
+#     return branch
+#
+# target = max(tree)
+#
+# while 1 :
+#     if branch(target) < m :
+#         target -= 1
+#     elif branch(target) >=m :
+#         print(target)
+#         break
 
-tree.sort()
 
 
-def branch(target) :
-    branch = 0
-    for i in range(0, n):
-        if tree[i] <= target :
-            continue
-        elif tree[i] > target :
-            branch += (tree[i] - target)
+def bin_search(tree) :
+    start = 0
+    end = max(tree)
+    while start <= end :
+        mid = (start + end) // 2
 
-    return branch
+        sum = 0
+        for i in range(0, n) :
+            if tree[i] > mid :
+                sum += tree[i] - mid
 
-target = tree[n-1]
+        if sum < m :
+            end = mid - 1
+        else :
+            start = mid + 1
 
-while 1 :
-    if branch(target) < m :
-        target -= 1
-    elif branch(target) >=m :
-        print(target)
-        break
+    print(end)
+
+bin_search(tree)
