d0360cc15db301a78c56350bdbac8a3fef24a856
Hyeji <heji0826@naver.com>
2021-07-31 02:48:16 +0900

    이것이 취업을 위한 코딩테스트다_success_upgrade

--- Hyeji/Greedy/이것이 취업을 위한 코딩테스트다_Q. 큰 수의 법칙.py
+++ Hyeji/Greedy/이것이 취업을 위한 코딩테스트다_Q. 큰 수의 법칙.py
@@ -19,3 +19,19 @@ while M > 0:
 
 print(result)
 
+# another method
+N, M, K = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[N-1] # 가장 큰 수
+second = data[N-2] # 두번째로 큰 수
+
+# 가장 큰 수가 더해지는 횟수
+count = int(M / (K+1)) * K
+count += M%(K+1)
+
+result = 0
+result += (count) * first
+result += (M - count) * second
+print(result)
