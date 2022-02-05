05997267b4ca9220ddd34cccaa85689e1a34c910
JooYeon <48883581+2JooYeon@users.noreply.github.com>
2022-02-05 18:24:44 +0900

    BaekJoon_플로이드_success

---
+++ JooYeon/baekjoon_Q11404.py
@@ -0,0 +1,30 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+INF = int(1e9)
+graph = [[INF for _ in range(n+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if graph[a][b] == INF:
+        graph[a][b] = c
+    else:
+        graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
