6dfa844201e7212a5efb5ed615ffe93489662037
JooYeon <48883581+2JooYeon@users.noreply.github.com>
2022-02-06 19:29:27 +0900

    Programmers_순위 검색_success

--- JooYeon/programmers_Q72412.py
+++ JooYeon/programmers_Q72412.py
@@ -1,20 +1,32 @@
+from itertools import combinations
+from bisect import bisect_left
+
 def solution(info, query):
     answer = []
-    
-    for q in query:
-        q = q.replace('and', '').split()
-        count = 0
-        for n in info:
-            satisfy = 1
-            n = n.split()
-            target = list(zip(q, n))
-            for a, b in target[:-1]:
-                if a == '-': continue
-                if a!=b:
-                    satisfy = 0
-                    break
-            if satisfy and int(target[-1][0])<=int(target[-1][1]):
-                count += 1
-        answer.append(count)
+    data = {}
+    for n in info:
+        n = n.split()
+        target = n[:-1]
+        score = int(n[-1])
+        for i in range(5):
+            for c in combinations(target, i):
+                key = ''.join(c)
+                if key in data:
+                    data[key].append(score)
+                else:
+                    data[key] = [score]
+    for key in data:
+        data[key].sort()
         
+    for q in query:
+        q = q.replace('and', '').replace('-', '').split()
+        cond = q[:-1]
+        cond_s = ''.join(cond)
+        cut = int(q[-1])
+        if cond_s in data:
+            index = bisect_left(data[cond_s], cut)
+            answer.append(len(data[cond_s])-index)
+        else:
+            answer.append(0)
+    
     return answer
