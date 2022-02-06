10c6e5deb24249db5c3b3733b0b648e01bd2c8c3
JooYeon <48883581+2JooYeon@users.noreply.github.com>
2022-02-07 00:20:18 +0900

    Programmers_가사 검색_success

--- JooYeon/programmers_Q60060.py
+++ JooYeon/programmers_Q60060.py
@@ -1,37 +1,42 @@
+from bisect import bisect_left, bisect_right
 def solution(words, queries):
     answer = []
-    s_data = {}
-    e_data = {}
+    
+    def count_by_range(data, left, right):
+        l_index = bisect_left(data, left)
+        r_index = bisect_right(data, right)
+        return r_index - l_index
+    
+    data = {}
+    r_data = {}
     for word in words:
         len_w = len(word)
-        for i in range(len_w):
-            key = (word[:i], len_w)
-            if key not in s_data:
-                s_data[key] = 1
-            else:
-                s_data[key] += 1
-            key = (word[i:], len_w)
-            if key not in e_data:
-                e_data[key] = 1
-            else:
-                e_data[key] += 1
+        if len_w not in data:
+            data[len_w] = [word]
+            r_data[len_w] = [word[::-1]]
+        else:
+            data[len_w].append(word)
+            r_data[len_w].append(word[::-1])
+            
+    for key in data:
+        data[key].sort()
+        r_data[key].sort()
+        
     for q in queries:
         len_q = len(q)
-        target = q.replace('?', '')
-        if not len(target) and (target, len_q) in s_data:
-            answer.append(s_data[(target, len_q)])
-            continue
-        if q[0] == '?':
-            q = q.replace('?', '')
-            if (q, len_q) in e_data:
-                answer.append(e_data[(q, len_q)])
+        if q[0] != '?':
+            left = q.replace('?', 'a')
+            right = q.replace('?', 'z')
+            if len_q in data:
+                answer.append(count_by_range(data[len_q], left, right))
             else:
                 answer.append(0)
         else:
-            q = q.replace('?', '')
-            if (q, len_q) in s_data:
-                answer.append(s_data[(q, len_q)])
+            left = q.replace('?', 'a')[::-1]
+            right = q.replace('?', 'z')[::-1]
+            if len_q in r_data:
+                answer.append(count_by_range(r_data[len_q], left, right))
             else:
                 answer.append(0)
-            
+    
     return answer
