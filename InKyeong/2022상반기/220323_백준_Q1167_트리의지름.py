ec3f88d7e59545d1561800ba77084a57b2cd9517
InKyeongBae <inkkyung@gmail.com>
2022-03-25 07:31:55 +0900

    Baekjoon_Q1167_트리의지름_triangle

--- InKyeong/2022상반기/220323_백준_Q1167_트리의지름.py
+++ InKyeong/2022상반기/220323_백준_Q1167_트리의지름.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 vertex = int(input())
 graph = [ [] for _ in range (vertex + 1) ]
 
@@ -6,27 +8,25 @@ for _ in range (vertex) :
 	for e in range(1, len(c) - 2, 2):
 		graph[c[0]].append((c[e], c[e + 1]))
 
-#dfs 들어가서 배열에 도착하는 곳까지의 거리 저장
-def dfs(v,result):
-	for a, b in graph[v]:
-		if result[a] == -1:
-			result[a] = result[v] + b
-			dfs(a,result)
+def bfs(start):
+	visit = [-1] * (vertex + 1)
+	que = deque()
+	que.append(start)
+	visit[start] = 0
+	_max = [0, 0]
 
-#임의의 점에서 최대 거리 저장
-result1 = [-1] * (vertex + 1)
-result1[1] = 0
-dfs(1, result1)
+	while que:
+		t = que.popleft()
+		for e, w in graph[t]:
+			if visit[e] == -1:
+				visit[e] = visit[t] + w
+				que.append(e)
+				if _max[0] < visit[e]:
+					_max = visit[e], e
 
-temp = 0
-index = 0
-#최대값일때의 인덱스값을 찾음
-for i in range(1, len(result1)):
-	if result1[i] > temp:
-		temp = result1[i]
-		index = i
-#그 인덱스에서 시작하는 최대값 찾음
-result2 = [-1] * (n+1)
-result2[index] = 0
-dfs(index, result2)
-print(max(result2))
+	return _max
+
+
+dis, node = bfs(1)
+dis, node = bfs(node)
+print(dis)
